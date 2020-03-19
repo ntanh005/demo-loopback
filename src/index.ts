6a230cbe6fac4f459d23884fea2581797c4f35b8
Anh Nguyen <ntanh005@gmail.com>
2020-03-19 19:26:02 +0700

    test

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {Lb4DemoApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {Lb4DemoApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new Lb4DemoApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
