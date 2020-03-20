fdac548fa71f802777a718b5862032ad5861e9fd
ntanh005 <ntanh005@gmail.com>
2020-03-20 15:11:38 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,19 @@ import {ApplicationConfig} from '@loopback/core';
 
 export {Lb4DemoApplication};
 
-export async function main(options: ApplicationConfig = {}) {
+export async function main(options: ApplicationConfig = {
+  rest: {
+      cors: {
+          origin: '*',
+          methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+          preflightContinue: false,
+          optionsSuccessStatus: 204,
+          maxAge: 86400,
+          credentials: true,
+          allowedHeaders : ["Content-Type", "authorizationToken", "userId"]
+      },
+    },
+}) {
   const app = new Lb4DemoApplication(options);
   await app.boot();
   await app.start();
