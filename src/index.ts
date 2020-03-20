cf726afeb2075ecd4f041f1e2fbb864ad7437cbb
ntanh005 <ntanh005@gmail.com>
2020-03-20 16:37:27 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,8 @@ export {Lb4DemoApplication};
 
 export async function main(options: ApplicationConfig = {}) {
 
-  options.rest = {
-    cors: {
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-      maxAge: 55500,
-      credentials: true,
-      allowedHeaders: ["Content-Type", "authorizationToken", "userId"]
-    }
-  };
- //   console.log(options);
+  
+  console.log(options);
   const app = new Lb4DemoApplication(options);
   await app.boot();
   await app.start();
