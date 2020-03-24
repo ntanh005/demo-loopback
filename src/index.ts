9c0ac03e687fd1f76a1df58c53d0caa691a32ee6
NhonPhan <48816829+NhonPhan@users.noreply.github.com>
2020-03-24 09:59:10 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,6 @@ export {Lb4DemoApplication};
 export async function main( ) {
 
   const options = {
-    rest = {
-       cors: {
-          allowedHeaders: 'Content-Type,authorizationToken,userId'
-       }
-    }
   };
   
   
